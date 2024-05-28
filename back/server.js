4fa8d442c839e800af85cfed8bb308ecf3618c80
metalBread33 <kfoughty80@gmail.com>
2024-05-28 18:23:06 -0400

    Set up frontend and backend

---
+++ back/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const port = 5000;
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("API is running");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
