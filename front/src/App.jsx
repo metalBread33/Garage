fee68958f4b142f1735b283b84bff21d6f1c395a
metalBread33 <kfoughty80@gmail.com>
2024-05-28 21:49:34 -0400

    Create basic homescreen and header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import {Container} from 'react-bootstrap'
+import {Outlet} from 'react-router-dom'
 import Header from "./components/Header.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <>
         <Header/>
+        <main className='py-3'>
+            <Container>
+                <Outlet/>
+            </Container>
+        </main>
       </>
   )
 }
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements} from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import HomeScreen from './screens/Home.jsx'
+
+const router = createBrowserRouter (
+    createRoutesFromElements(
+        <Route path='/' element={<App/>}>
+            <Route index={true} path='/' element={<HomeScreen/>}/>
+        </Route>
+    )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
